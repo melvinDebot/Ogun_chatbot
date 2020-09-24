b24a463ec21d1342a411fea1642998c675faec12
melvinDebot <melvindebot@gmail.com>
2020-09-24 18:05:08 +0200

    tentative logo mais louper

---
+++ src/components/Particles.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div v-for="item in particles" :key="item.id"></div>
+    <li >
+    {{ item.message }}
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Particle',
+  data: ()=> {
+    return {
+      particles : [
+        {
+          id:0,
+          color:"red"
+        },
+        {
+          id:1,
+          color:"green"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
